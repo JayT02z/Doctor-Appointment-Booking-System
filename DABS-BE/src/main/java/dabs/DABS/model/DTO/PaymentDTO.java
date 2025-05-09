7799865ebc54cb8a11944aa95b0b3b7b30b84ac0
Ho4FireDragon <anhoa1794@gmail.com>
2025-05-09 22:17:05 +0700

    Hotfix

--- DABS-BE/src/main/java/dabs/DABS/model/DTO/PaymentDTO.java
+++ DABS-BE/src/main/java/dabs/DABS/model/DTO/PaymentDTO.java
@@ -10,12 +10,14 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PaymentDTO {
+    private Long id;
     private AppointmentDTO appointment;
     private Double amount;
     private PaymentMethod paymentMethod;
     private PaymentStatus status;
 
     public PaymentDTO(Payment payment) {
+        this.id = payment.getId();
         this.appointment = new AppointmentDTO(payment.getAppointment());
         this.amount = payment.getAmount();
         this.paymentMethod = payment.getPaymentMethod();
